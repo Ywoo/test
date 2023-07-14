bcc9f4faae7f69e433d7e310e0257a0bb9eeda73
ywoo <ywoo@github.com>
2023-07-14 16:42:55 +0900

    second version

--- a.py
+++ a.py
@@ -1,5 +1,9 @@
 # this file is for testing git merge
 # will show the exact author for each line
 
+# ywoo add new line
+
 def test_message():
+	''' print test message
+	'''
 	return print('Hello World')
